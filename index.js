eb972a9660c11641d67bf2253f2e979d7128471d
BennyJiangzy <Bennyjiangzy@gmail.com>
2021-09-28 10:48:31 -0700

    file link change

--- index.js
+++ index.js
@@ -15,6 +15,26 @@ app.get("/project", (req, res) => {
     res.sendFile(path.join(__dirname,"pages/project.html"))
 })
 
+app.get("/Me", (req, res) => {
+  
+    res.sendFile(path.join(__dirname,"public/Benny.jpg"))
+})
+
+app.get("/Resume", (req, res) => {
+  
+    res.sendFile(path.join(__dirname,"public/Benny Resume.pdf"))
+})
+
+app.get("/Happy", (req, res) => {
+  
+    res.sendFile(path.join(__dirname,"public/Happy.png"))
+})
+
+app.get("/Pygame", (req, res) => {
+  
+    res.sendFile(path.join(__dirname,"public/Pygame.png"))
+})
+
 
 
 app.listen(8080, () => console.log("listening on port 8080"))
