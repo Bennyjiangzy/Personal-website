bc1469704d05c9b835e2c43c4b26db2dc495aa44
Bennyjiangzy <bennyjiangzy@gmail.com>
2021-09-27 23:41:56 -0700

    initial version

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+app.use(express.static('public'));
+app.use('/static', express.static('public'));
+
+app.get("/", (req, res) => {
+  
+    res.sendFile(path.join(__dirname,"pages/Benny.html"))
+})
+
+app.get("/project", (req, res) => {
+  
+    res.sendFile(path.join(__dirname,"pages/project.html"))
+})
+
+
+
+app.listen(8080, () => console.log("listening on port 8080"))
